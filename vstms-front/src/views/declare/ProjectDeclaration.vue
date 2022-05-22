3b855705b759b4647ace0d82d2d443399af0a460
iliweii <chall21@yeah.net>
2022-05-23 02:48:44 +0800

    项目申报书

---
+++ vstms-front/src/views/declare/ProjectDeclaration.vue
@@ -0,0 +1,34 @@
+<template>
+  <page-layout :title="title">
+    <a-card :bordered="false">
+      <j-file-upload
+        ref="jFileUpload"
+        :disabled="false"
+        businessType="declare_form"
+        objectId="declare_form"
+        dict="file_declare_form"
+        :labelColumn="{ span: 0 }"
+        :wrapperColumn="{ span: 24 }"
+      />
+    </a-card>
+  </page-layout>
+</template>
+
+<script>
+  import PageLayout from '@/components/page/PageLayout'
+
+  export default {
+    name: 'ProjectDeclaration',
+    components: {
+      PageLayout
+    },
+    data () {
+      return {
+        title: '项目申报书'
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+</style>

742519fcc73e647d14abdb43fbcf62c33ab14267
iliweii <chall21@yeah.net>
2022-05-24 05:45:43 +0800

    资料领取界面

---
+++ vstms-front/src/views/training/TrainingReceiveList.vue
@@ -0,0 +1,158 @@
+<template>
+  <page-layout :title="title">
+    <a-card :bordered="false">
+      <!-- 查询区域 -->
+      <div class="table-page-search-wrapper">
+        <a-form layout="inline" @keyup.enter.native="searchQuery">
+          <a-row :gutter="24">
+            <a-col :xl="6" :lg="7" :md="8" :sm="24">
+              <a-form-item label="培训班" required>
+                <j-search-select-tag
+                  v-model="queryParam.classNo"
+                  :triggerChange="true"
+                  placeholder="请选择培训班"
+                  dict="training_class,name,no"
+                  :disabled="false"
+                  @change="loadData()"
+                />
+              </a-form-item>
+            </a-col>
+            <a-col :xl="6" :lg="7" :md="8" :sm="24">
+              <a-form-item label="领取状态">
+                <j-search-select-tag
+                  v-model="queryParam.status"
+                  :triggerChange="true"
+                  placeholder="请选择领取状态"
+                  dict="training_receive_status"
+                  :disabled="false"
+                  @change="loadData()"
+                />
+              </a-form-item>
+            </a-col>
+
+            <a-col :xl="6" :lg="7" :md="8" :sm="24">
+              <span style="float: left; overflow: hidden" class="table-page-search-submitButtons">
+                <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
+                <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
+                <a @click="handleToggleSearch" style="margin-left: 8px">
+                  {{ toggleSearchStatus ? '收起' : '展开' }}
+                  <a-icon :type="toggleSearchStatus ? 'up' : 'down'" />
+                </a>
+              </span>
+            </a-col>
+            <template v-if="toggleSearchStatus"> </template>
+          </a-row>
+        </a-form>
+      </div>
+
+      <!-- 隐藏的Table -->
+      <a-table
+        v-show="false"
+        ref="table"
+        rowKey="id"
+        :columns="columns"
+        :dataSource="dataSource"
+        :pagination="ipagination"
+        :loading="loading"
+        @change="handleTableChange"
+      >
+      </a-table>
+
+      <!-- 区域-begin -->
+      <div v-if="queryParam.classNo" :style="{ display: 'flex' }">
+        <div v-for="item in dataSource" :key="item.id" :style="{ padding: '5px 10px' }">
+          <a-card hoverable style="width: 150px">
+            <img slot="cover" alt="头像" :src="item.avatar" v-if="item.avatar" />
+            <img
+              slot="cover"
+              alt="头像"
+              src="https://ss0.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=57917317,3261650226&fm=26&gp=0.jpg"
+              v-else
+            />
+            <a-card-meta :title="item.username_dictText">
+              <template slot="description">
+                <span>当前：{{ item.status_dictText }}</span>
+                <a-popconfirm title="确定标记为已领取吗?" @confirm="() => handleMark(item)" v-if="item.status == '0'">
+                  <a-button type="link" size="small">标记为已领取</a-button>
+                </a-popconfirm>
+              </template>
+            </a-card-meta>
+          </a-card>
+        </div>
+      </div>
+      <a-empty v-else description="请先选择一个培训班，再进行操作" />
+      <!-- table区域-end -->
+
+      <!-- 表单区域 -->
+      <trainingReceive-modal ref="modalForm" @ok="modalFormOk"></trainingReceive-modal>
+    </a-card>
+  </page-layout>
+</template>
+
+<script>
+import '@/assets/less/TableExpand.less'
+import PageLayout from '@/components/page/PageLayout'
+import TrainingReceiveModal from './modules/TrainingReceiveModal'
+import { putAction } from '@/api/manage'
+import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+
+export default {
+  name: 'TrainingReceiveList',
+  mixins: [JeecgListMixin],
+  components: {
+    PageLayout,
+    TrainingReceiveModal,
+  },
+  data() {
+    return {
+      description: '资料领取管理页面',
+      title: '培训班资料领取',
+      url: {
+        list: '/training/trainingReceive/list',
+        edit: '/training/trainingReceive/edit',
+        delete: '/training/trainingReceive/delete',
+        deleteBatch: '/training/trainingReceive/deleteBatch',
+        exportXlsUrl: 'training/trainingReceive/exportXls',
+        importExcelUrl: 'training/trainingReceive/importExcel',
+      },
+      columns: [
+        {
+          title: '#',
+          dataIndex: '',
+          key: 'rowIndex',
+          width: 60,
+          align: 'center',
+          customRender: function (t, r, index) {
+            return parseInt(index) + 1
+          },
+        },
+      ],
+      /* 分页参数 */
+      ipagination: {
+        current: 1,
+        pageSize: -1,
+        pageSizeOptions: ['10', '20', '30'],
+        showTotal: (total, range) => {
+          return range[0] + '-' + range[1] + ' 共' + total + '条'
+        },
+        showQuickJumper: true,
+        showSizeChanger: true,
+        total: 0,
+      },
+    }
+  },
+  methods: {
+    handleMark(item) {
+      const that = this
+      item.status = '1'
+      putAction(that.url.edit, item).then((res) => {
+        that.$message.success('操作成功')
+        that.loadData()
+      })
+    },
+  },
+}
+</script>
+<style scoped>
+@import '~@assets/less/common.less';
+</style>

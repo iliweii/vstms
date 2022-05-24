ffea2e6bc771ce55c19a9c3634350ef02f40dd50
iliweii <chall21@yeah.net>
2022-05-25 06:59:03 +0800

    培训日志界面

---
+++ vstms-front/src/views/logg/TeachingList.vue
@@ -0,0 +1,173 @@
+<template>
+  <page-layout :title="title">
+    <a-card :bordered="false">
+      <!-- 查询区域 -->
+      <div class="table-page-search-wrapper">
+        <a-form layout="inline" @keyup.enter.native="searchQuery">
+          <a-row :gutter="24">
+            <a-col :xl="6" :lg="7" :md="8" :sm="24">
+              <a-form-item label="日志内容">
+                <j-input placeholder="请输入日志内容" v-model="queryParam.content"></j-input>
+              </a-form-item>
+            </a-col>
+            <a-col :xl="6" :lg="7" :md="8" :sm="24">
+              <span style="float: left; overflow: hidden" class="table-page-search-submitButtons">
+                <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
+                <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
+                <a @click="handleToggleSearch" style="margin-left: 8px">
+                  {{ toggleSearchStatus ? '收起' : '展开' }}
+                  <a-icon :type="toggleSearchStatus ? 'up' : 'down'" />
+                </a>
+              </span>
+            </a-col>
+            <template v-if="toggleSearchStatus"> </template>
+          </a-row>
+        </a-form>
+      </div>
+
+      <!-- 操作按钮区域 -->
+      <div class="table-operator">
+        <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
+        <a-button type="primary" icon="download" @click="handleExportXls('授课教师日志')">导出</a-button>
+        <a-dropdown v-if="selectedRowKeys.length > 0">
+          <a-menu slot="overlay">
+            <a-menu-item key="1" @click="batchDel"><a-icon type="delete" />删除</a-menu-item>
+          </a-menu>
+          <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>
+        </a-dropdown>
+      </div>
+
+      <!-- table区域-begin -->
+      <div>
+        <div class="ant-alert ant-alert-info" style="margin-bottom: 16px">
+          <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择
+          <a style="font-weight: 600">{{ selectedRowKeys.length }}</a
+          >项
+          <a style="margin-left: 24px" @click="onClearSelected">清空</a>
+        </div>
+
+        <a-table
+          ref="table"
+          size="middle"
+          bordered
+          rowKey="id"
+          :columns="columns"
+          :dataSource="dataSource"
+          :pagination="ipagination"
+          :loading="loading"
+          class="j-table-force-nowrap"
+          :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
+          @change="handleTableChange"
+        >
+          <span slot="action" slot-scope="text, record">
+            <a @click="handleDetail(record)">查看</a>
+
+            <a-divider type="vertical" />
+            <a-dropdown>
+              <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>
+              <a-menu slot="overlay">
+                <a-menu-item>
+                  <a @click="handleEdit(record)">编辑</a>
+                </a-menu-item>
+                <a-menu-item>
+                  <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
+                    <a>删除</a>
+                  </a-popconfirm>
+                </a-menu-item>
+              </a-menu>
+            </a-dropdown>
+          </span>
+        </a-table>
+      </div>
+      <!-- table区域-end -->
+
+      <!-- 表单区域 -->
+      <trainingLog-modal ref="modalForm" @ok="modalFormOk"></trainingLog-modal>
+    </a-card>
+  </page-layout>
+</template>
+
+<script>
+import '@/assets/less/TableExpand.less'
+import PageLayout from '@/components/page/PageLayout'
+import TrainingLogModal from './modules/TeachingModal'
+import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+
+export default {
+  name: 'TeachingList',
+  mixins: [JeecgListMixin],
+  components: {
+    PageLayout,
+    TrainingLogModal,
+  },
+  data() {
+    return {
+      description: '培训日志管理页面',
+      title: '授课教师日志',
+      // 表头
+      columns: [
+        {
+          title: '#',
+          dataIndex: '',
+          key: 'rowIndex',
+          width: 60,
+          align: 'center',
+          customRender: function (t, r, index) {
+            return parseInt(index) + 1
+          },
+        },
+        {
+          title: '日志编号',
+          align: 'center',
+          dataIndex: 'no',
+          ellipsis: true,
+          width: 120,
+        },
+        {
+          title: '日志内容',
+          align: 'center',
+          dataIndex: 'content',
+          ellipsis: true,
+        },
+        {
+          title: '创建人',
+          align: 'center',
+          dataIndex: 'createBy_dictText',
+          ellipsis: true,
+          width: 120,
+        },
+        {
+          title: '创建时间',
+          align: 'center',
+          dataIndex: 'createTime',
+          ellipsis: true,
+          width: 120,
+        },
+        {
+          title: '操作',
+          dataIndex: 'action',
+          align: 'center',
+          scopedSlots: { customRender: 'action' },
+          width: 180,
+        },
+      ],
+      url: {
+        list: '/training/trainingLog/list?flag=1&type=2',
+        delete: '/training/trainingLog/delete',
+        deleteBatch: '/training/trainingLog/deleteBatch',
+        exportXlsUrl: 'training/trainingLog/exportXls',
+        importExcelUrl: 'training/trainingLog/importExcel',
+      },
+    }
+  },
+  computed: {
+    importExcelUrl: function () {
+      return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`
+    },
+  },
+  methods: {},
+}
+</script>
+<style scoped>
+@import '~@assets/less/common.less';
+</style>
---
+++ vstms-front/src/views/logg/modules/TeachingModal.vue
@@ -0,0 +1,122 @@
+<template>
+  <a-drawer :title="title" width="60%" placement="right" :closable="false" @close="close" :visible="visible">
+    <a-spin :spinning="confirmLoading">
+      <a-form-model ref="form" :model="model" :rules="validatorRules">
+        <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" prop="content" label="授课日志">
+          <j-editor v-model="model.content" autoSize :style="{ height: '500px' }" :disabled="disableSubmit" />
+        </a-form-model-item>
+      </a-form-model>
+    </a-spin>
+
+    <div class="drawer-bootom-button">
+      <a-button type="primary" @click="handleOk" :disabled="disableSubmit">确定</a-button>
+      <a-button type="primary" @click="handleCancel">取消</a-button>
+    </div>
+  </a-drawer>
+</template>
+
+<script>
+import { httpAction } from '@/api/manage'
+import moment from 'moment'
+
+export default {
+  name: 'TeachingModal',
+  data() {
+    return {
+      title: '操作',
+      visible: false,
+      model: {},
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 2 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 20 },
+      },
+
+      disableSubmit: false,
+      confirmLoading: false,
+      validatorRules: {},
+      url: {
+        add: '/training/trainingLog/add',
+        edit: '/training/trainingLog/edit',
+      },
+    }
+  },
+  created() {},
+  methods: {
+    add() {
+      //初始化默认值
+      this.edit({})
+    },
+    edit(record) {
+      this.model = Object.assign({}, record)
+      this.visible = true
+    },
+    close() {
+      this.$emit('close')
+      this.visible = false
+      this.$refs.form.clearValidate()
+    },
+    handleOk() {
+      const that = this
+      // 触发表单验证
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          that.confirmLoading = true
+          let httpurl = ''
+          let method = ''
+          if (!this.model.id) {
+            httpurl += this.url.add
+            method = 'post'
+          } else {
+            httpurl += this.url.edit
+            method = 'put'
+          }
+          this.model.type = '2' // 授课教师日志
+          httpAction(httpurl, this.model, method)
+            .then((res) => {
+              if (res.success) {
+                that.$message.success(res.message)
+                that.$emit('ok')
+              } else {
+                that.$message.warning(res.message)
+              }
+            })
+            .finally(() => {
+              that.confirmLoading = false
+              that.close()
+            })
+        } else {
+          return false
+        }
+      })
+    },
+    handleCancel() {
+      this.close()
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+/**Button按钮间距*/
+.ant-btn {
+  margin-left: 30px;
+  margin-bottom: 30px;
+  float: right;
+}
+/**抽屉按钮样式*/
+.drawer-bootom-button {
+  position: absolute;
+  bottom: -8px;
+  width: 100%;
+  border-top: 1px solid #e8e8e8;
+  padding: 10px 16px;
+  text-align: right;
+  left: 0;
+  background: #fff;
+  border-radius: 0 0 2px 2px;
+}
+</style>

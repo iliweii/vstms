7e82738b7e3d5e66a1ed29be777b39ea2b55e80d
iliweii <chall21@yeah.net>
2022-05-30 08:26:56 +0800

    考勤管理前端页面完善

--- vstms-front/src/views/training/TrainingAttendanceList.vue
+++ vstms-front/src/views/training/TrainingAttendanceList.vue
@@ -1,67 +1,111 @@
 <template>
   <a-card :bordered="false">
-
     <!-- 查询区域 -->
     <div class="table-page-search-wrapper">
       <a-form layout="inline" @keyup.enter.native="searchQuery">
         <a-row :gutter="24">
-
           <a-col :xl="6" :lg="7" :md="8" :sm="24">
-            <a-form-item label="培训班编号">
-              <a-input placeholder="请输入培训班编号" v-model="queryParam.classNo"></a-input>
+            <a-form-item label="培训班">
+              <j-search-select-tag
+                v-model="queryParam.classNo"
+                :triggerChange="true"
+                placeholder="请选择培训班"
+                dict="training_class,name,no"
+                :disabled="false"
+                @change="loadData()"
+              />
             </a-form-item>
           </a-col>
-          <a-col :xl="6" :lg="7" :md="8" :sm="24">
-            <a-form-item label="学员用户名">
-              <a-input placeholder="请输入学员用户名" v-model="queryParam.username"></a-input>
-            </a-form-item>
-          </a-col>
-        <template v-if="toggleSearchStatus">
           <a-col :xl="6" :lg="7" :md="8" :sm="24">
             <a-form-item label="考勤日期">
-              <a-input placeholder="请输入考勤日期" v-model="queryParam.atdDate"></a-input>
+              <j-date
+                placeholder="请选择考勤日期"
+                style="width: 100%"
+                v-model="queryParam.atdDate"
+                :showTime="true"
+                dateFormat="YYYY-MM-DD"
+                @change="loadData()"
+              />
             </a-form-item>
           </a-col>
           <a-col :xl="6" :lg="7" :md="8" :sm="24">
-            <a-form-item label="考勤状态 0出勤 1迟到 2旷课 3请假">
-              <a-input placeholder="请输入考勤状态 0出勤 1迟到 2旷课 3请假" v-model="queryParam.atdStatus"></a-input>
+            <a-form-item label="考勤状态">
+              <j-search-select-tag
+                v-model="queryParam.atdStatus"
+                :triggerChange="true"
+                placeholder="请选择考勤状态"
+                dict="training_attendance_status"
+                :disabled="false"
+                @change="loadData()"
+              />
             </a-form-item>
           </a-col>
-          </template>
           <a-col :xl="6" :lg="7" :md="8" :sm="24">
-            <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+            <span style="float: left; overflow: hidden" class="table-page-search-submitButtons">
               <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
               <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
               <a @click="handleToggleSearch" style="margin-left: 8px">
                 {{ toggleSearchStatus ? '收起' : '展开' }}
-                <a-icon :type="toggleSearchStatus ? 'up' : 'down'"/>
+                <a-icon :type="toggleSearchStatus ? 'up' : 'down'" />
               </a>
             </span>
           </a-col>
-
         </a-row>
       </a-form>
     </div>
 
     <!-- 操作按钮区域 -->
-    <div class="table-operator">
-      <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
-      <a-button type="primary" icon="download" @click="handleExportXls('考勤管理')">导出</a-button>
-      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+    <div class="table-operator" v-if="queryParam.classNo">
+      <!-- <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button> -->
+      <!-- <a-button type="primary" icon="download" @click="handleExportXls('考勤管理')">导出</a-button> -->
+      <a-upload
+        name="file"
+        :showUploadList="false"
+        :multiple="false"
+        :headers="tokenHeader"
+        :action="importExcelUrl"
+        @change="handleImportExcel"
+      >
         <a-button type="primary" icon="import">导入</a-button>
       </a-upload>
+      <a-button type="primary" icon="download" @click="handleExportTemp('考勤表')">导出考勤表模板</a-button>
+      <a-button type="primary" icon="download" @click="handleExportError('考勤表导入错误信息')">导出错误信息</a-button>
       <a-dropdown v-if="selectedRowKeys.length > 0">
         <a-menu slot="overlay">
-          <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>
+          <a-menu-item key="1" @click="batchDel"><a-icon type="delete" />删除</a-menu-item>
         </a-menu>
         <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>
       </a-dropdown>
+
+      <a-alert
+        v-if="!uploadStatus"
+        :description="
+          queryParam.atdDate
+            ? `${queryParam.atdDate}日考勤表未上传`
+            : '今日还未上传本培训班的考勤表，请及时下载考勤模板，填写考勤情况，并上传考勤表。'
+        "
+        type="error"
+        closable
+      />
+      <a-alert
+        v-else
+        :message="`${
+          queryParam.atdDate ? queryParam.atdDate + '日' : '今日'
+        }考勤已上传，本培训班共 ${totalNum}人，出勤 ${attendanceNum}人，出勤率 ${(
+          (attendanceNum / totalNum) *
+          100
+        ).toFixed(0)}%。`"
+        type="success"
+        show-icon
+      />
     </div>
 
     <!-- table区域-begin -->
-    <div>
-      <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
-        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
+    <div v-if="queryParam.classNo">
+      <div class="ant-alert ant-alert-info" style="margin-bottom: 16px">
+        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择
+        <a style="font-weight: 600">{{ selectedRowKeys.length }}</a
+        >项
         <a style="margin-left: 24px" @click="onClearSelected">清空</a>
       </div>
 
@@ -75,9 +119,12 @@
         :pagination="ipagination"
         :loading="loading"
         class="j-table-force-nowrap"
-        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
-        @change="handleTableChange">
-
+        :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
+        @change="handleTableChange"
+      >
+        <span slot="atdStatus" slot-scope="text, record">
+          <a-tag :color="['green', 'pink', 'red', 'orange'][record.atdStatus]"> {{ text }} </a-tag>
+        </span>
         <span slot="action" slot-scope="text, record">
           <a @click="handleEdit(record)">编辑</a>
 
@@ -93,9 +140,9 @@
             </a-menu>
           </a-dropdown>
         </span>
-
       </a-table>
     </div>
+    <a-empty v-else description="请先选择一个培训班，再进行操作" />
     <!-- table区域-end -->
 
     <!-- 表单区域 -->
@@ -104,77 +151,189 @@
 </template>
 
 <script>
-  import '@/assets/less/TableExpand.less'
-  import TrainingAttendanceModal from './modules/TrainingAttendanceModal'
-  import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+import Vue from 'vue'
+import '@/assets/less/TableExpand.less'
+import TrainingAttendanceModal from './modules/TrainingAttendanceModal'
+import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+import { deleteAction, getAction, downFile, getFileAccessHttpUrl } from '@/api/manage'
 
-  export default {
-    name: "TrainingAttendanceList",
-    mixins:[JeecgListMixin],
-    components: {
-      TrainingAttendanceModal
-    },
-    data () {
-      return {
-        description: '考勤管理管理页面',
-        // 表头
-        columns: [
-          {
-            title: '#',
-            dataIndex: '',
-            key:'rowIndex',
-            width:60,
-            align:"center",
-            customRender:function (t,r,index) {
-              return parseInt(index)+1;
-            }
-           },
-		   {
-            title: '培训班编号',
-            align:"center",
-            dataIndex: 'classNo'
-           },
-		   {
-            title: '学员用户名',
-            align:"center",
-            dataIndex: 'username'
-           },
-		   {
-            title: '考勤日期',
-            align:"center",
-            dataIndex: 'atdDate'
-           },
-		   {
-            title: '考勤状态 0出勤 1迟到 2旷课 3请假',
-            align:"center",
-            dataIndex: 'atdStatus'
-           },
-          {
-            title: '操作',
-            dataIndex: 'action',
-            align:"center",
-            scopedSlots: { customRender: 'action' },
-          }
-        ],
-		url: {
-          list: "/training/trainingAttendance/list",
-          delete: "/training/trainingAttendance/delete",
-          deleteBatch: "/training/trainingAttendance/deleteBatch",
-          exportXlsUrl: "training/trainingAttendance/exportXls",
-          importExcelUrl: "training/trainingAttendance/importExcel",
-       },
+export default {
+  name: 'TrainingAttendanceList',
+  mixins: [JeecgListMixin],
+  components: {
+    TrainingAttendanceModal,
+  },
+  data() {
+    return {
+      description: '考勤管理管理页面',
+      uploadStatus: false, // 今日上传状态
+      totalNum: 0, // 培训班总人数
+      attendanceNum: 0, // 已考勤人数
+      // 表头
+      columns: [
+        {
+          title: '#',
+          dataIndex: '',
+          key: 'rowIndex',
+          width: 60,
+          align: 'center',
+          customRender: function (t, r, index) {
+            return parseInt(index) + 1
+          },
+        },
+        {
+          title: '培训班编号',
+          align: 'center',
+          dataIndex: 'classNo',
+        },
+        {
+          title: '学员用户名',
+          align: 'center',
+          dataIndex: 'username',
+        },
+        {
+          title: '学员姓名',
+          align: 'center',
+          dataIndex: 'username_dictText',
+          sorter: (a, b) => a.username - b.username,
+        },
+        {
+          title: '考勤日期',
+          align: 'center',
+          dataIndex: 'atdDate',
+          sorter: (a, b) => a.atdDate - b.atdDate,
+        },
+        {
+          title: '考勤状态',
+          align: 'center',
+          dataIndex: 'atdStatus_dictText',
+          sorter: (a, b) => a.atdStatus - b.atdStatus,
+          scopedSlots: { customRender: 'atdStatus' },
+        },
+        {
+          title: '操作',
+          dataIndex: 'action',
+          align: 'center',
+          scopedSlots: { customRender: 'action' },
+        },
+      ],
+      url: {
+        list: '/training/trainingAttendance/list',
+        delete: '/training/trainingAttendance/delete',
+        deleteBatch: '/training/trainingAttendance/deleteBatch',
+        exportXlsUrl: 'training/trainingAttendance/exportXls',
+        errorXls: 'training/trainingAttendance/errorXls',
+        exportTemplate: 'training/trainingAttendance/exportTemplate',
+        importExcelUrl: 'training/trainingAttendance/importExcel',
+      },
     }
   },
   computed: {
-    importExcelUrl: function(){
-      return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
-    }
+    importExcelUrl: function () {
+      return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`
+    },
   },
-    methods: {
-     
-    }
-  }
+  created() {
+    this.init()
+    this.loadData()
+  },
+  methods: {
+    /**
+     * 界面初始化时调用
+     */
+    init() {
+      // 将用户习惯的查询取出
+      Object.assign(this.queryParam, Vue.ls.get('USER_QUERY_CLASS_NO'))
+    },
+    /**
+     * 加载数据
+     * @param arg 参数
+     */
+    loadData(arg) {
+      if (arg === 1) {
+        this.ipagination.current = 1
+      }
+      var params = this.getQueryParams() //查询条件
+      // 将查询条件存储到本地
+      if (params.classNo) Vue.ls.set('USER_QUERY_CLASS_NO', { classNo: params.classNo })
+      else return
+      this.loading = true
+      getAction(this.url.list, params)
+        .then((res) => {
+          if (res.success) {
+            this.dataSource = res.result.records || res.result
+            if (res.message == 'false') this.uploadStatus = false
+            else {
+              let a = res.message.split('|')
+              this.uploadStatus = true
+              this.totalNum = a[1]
+              this.attendanceNum = a[2]
+            }
+            if (res.result.total) {
+              this.ipagination.total = res.result.total
+            } else {
+              this.ipagination.total = 0
+            }
+          } else {
+            this.$message.warning(res.message)
+          }
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
+    handleExportTemp(fileName) {
+      let param = this.getQueryParams()
+      console.log('导出参数', param)
+      downFile(this.url.exportTemplate, param).then((data) => {
+        if (!data) {
+          this.$message.warning('文件下载失败')
+          return
+        }
+        if (typeof window.navigator.msSaveBlob !== 'undefined') {
+          window.navigator.msSaveBlob(new Blob([data], { type: 'application/vnd.ms-excel' }), fileName + '.xls')
+        } else {
+          let url = window.URL.createObjectURL(new Blob([data], { type: 'application/vnd.ms-excel' }))
+          let link = document.createElement('a')
+          link.style.display = 'none'
+          link.href = url
+          link.setAttribute('download', fileName + '.xls')
+          document.body.appendChild(link)
+          link.click()
+          document.body.removeChild(link) //下载完成移除元素
+          window.URL.revokeObjectURL(url) //释放掉blob对象
+        }
+      })
+    },
+    handleExportError(fileName) {
+      let param = this.getQueryParams()
+      console.log('导出参数', param)
+      downFile(this.url.errorXls, param).then((data) => {
+        if (!data) {
+          this.$message.warning('文件下载失败')
+          return
+        } else if (data.type === 'application/json') {
+          this.$message.warning('暂无错误信息')
+          return
+        }
+        if (typeof window.navigator.msSaveBlob !== 'undefined') {
+          window.navigator.msSaveBlob(new Blob([data], { type: 'application/vnd.ms-excel' }), fileName + '.xls')
+        } else {
+          let url = window.URL.createObjectURL(new Blob([data], { type: 'application/vnd.ms-excel' }))
+          let link = document.createElement('a')
+          link.style.display = 'none'
+          link.href = url
+          link.setAttribute('download', fileName + '.xls')
+          document.body.appendChild(link)
+          link.click()
+          document.body.removeChild(link) //下载完成移除元素
+          window.URL.revokeObjectURL(url) //释放掉blob对象
+        }
+      })
+    },
+  },
+}
 </script>
 <style scoped>
-  @import '~@assets/less/common.less';
+@import '~@assets/less/common.less';
 </style>
--- vstms-front/src/views/training/modules/TrainingAttendanceModal.vue
+++ vstms-front/src/views/training/modules/TrainingAttendanceModal.vue
@@ -1,140 +1,143 @@
 <template>
-  <a-drawer
-      :title="title"
-      :width="800"
-      placement="right"
-      :closable="false"
-      @close="close"
-      :visible="visible">
-
+  <a-drawer :title="title" :width="800" placement="right" :closable="false" @close="close" :visible="visible">
     <a-spin :spinning="confirmLoading">
       <a-form-model ref="form" :model="model" :rules="validatorRules">
-      
         <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" prop="classNo" label="培训班编号">
-          <a-input placeholder="请输入培训班编号" v-model="model.classNo" />
+          <a-input placeholder="请输入培训班编号" v-model="model.classNo" disabled />
         </a-form-model-item>
         <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" prop="username" label="学员用户名">
-          <a-input placeholder="请输入学员用户名" v-model="model.username" />
+          <a-input placeholder="请输入学员用户名" v-model="model.username" disabled />
         </a-form-model-item>
         <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" prop="atdDate" label="考勤日期">
-          <a-date-picker showTime valueFormat='YYYY-MM-DD HH:mm:ss' v-model="model.atdDate" />
+          <j-date
+            placeholder="请选择考勤日期"
+            style="width: 100%"
+            v-model="model.atdDate"
+            :showTime="true"
+            dateFormat="YYYY-MM-DD"
+            :disabled="true"
+          />
         </a-form-model-item>
-        <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" prop="atdStatus" label="考勤状态 0出勤 1迟到 2旷课 3请假">
-          <a-input placeholder="请输入考勤状态 0出勤 1迟到 2旷课 3请假" v-model="model.atdStatus" />
+        <a-form-model-item :labelCol="labelCol" :wrapperCol="wrapperCol" prop="atdStatus" label="考勤状态">
+          <j-search-select-tag
+            v-model="model.atdStatus"
+            :triggerChange="true"
+            placeholder="请选择考勤状态"
+            dict="training_attendance_status"
+            :disabled="disableSubmit"
+          />
         </a-form-model-item>
-		
       </a-form-model>
     </a-spin>
 
     <div class="drawer-bootom-button">
-      <a-button type="primary" @click="handleOk">确定</a-button>
+      <a-button type="primary" @click="handleOk" :disabled="disableSubmit">确定</a-button>
       <a-button type="primary" @click="handleCancel">取消</a-button>
     </div>
   </a-drawer>
 </template>
 
 <script>
-  import { httpAction } from '@/api/manage'
-  import moment from "moment"
+import { httpAction } from '@/api/manage'
+import moment from 'moment'
 
-  export default {
-    name: "TrainingAttendanceModal",
-    data () {
-      return {
-        title:"操作",
-        visible: false,
-        model: {},
-        labelCol: {
-          xs: { span: 24 },
-          sm: { span: 5 },
-        },
-        wrapperCol: {
-          xs: { span: 24 },
-          sm: { span: 16 },
-        },
-
-        confirmLoading: false,
-        validatorRules:{
-        },
-        url: {
-          add: "/training/trainingAttendance/add",
-          edit: "/training/trainingAttendance/edit",
-        },
-      }
-    },
-    created () {
-    },
-    methods: {
-      add () {
-        //初始化默认值
-        this.edit({});
+export default {
+  name: 'TrainingAttendanceModal',
+  data() {
+    return {
+      title: '操作',
+      visible: false,
+      disableSubmit: false,
+      model: {},
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 5 },
       },
-      edit (record) {
-        this.model = Object.assign({}, record);
-        this.visible = true;
-      },
-      close () {
-        this.$emit('close');
-        this.visible = false;
-        this.$refs.form.clearValidate();
-      },
-      handleOk () {
-        const that = this;
-        // 触发表单验证
-        this.$refs.form.validate(valid => {
-          if (valid) {
-            that.confirmLoading = true;
-            let httpurl = '';
-            let method = '';
-            if(!this.model.id){
-              httpurl+=this.url.add;
-              method = 'post';
-            }else{
-              httpurl+=this.url.edit;
-               method = 'put';
-            }
-            httpAction(httpurl,this.model,method).then((res)=>{
-              if(res.success){
-                that.$message.success(res.message);
-                that.$emit('ok');
-              }else{
-                that.$message.warning(res.message);
-              }
-            }).finally(() => {
-              that.confirmLoading = false;
-              that.close();
-            })
-          }else{
-            return false;
-         }
-        })
-      },
-      handleCancel () {
-        this.close()
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
       },
 
-
+      confirmLoading: false,
+      validatorRules: {},
+      url: {
+        add: '/training/trainingAttendance/add',
+        edit: '/training/trainingAttendance/edit',
+      },
     }
-  }
+  },
+  created() {},
+  methods: {
+    add() {
+      //初始化默认值
+      this.edit({})
+    },
+    edit(record) {
+      this.model = Object.assign({}, record)
+      this.visible = true
+    },
+    close() {
+      this.$emit('close')
+      this.visible = false
+      this.$refs.form.clearValidate()
+    },
+    handleOk() {
+      const that = this
+      // 触发表单验证
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          that.confirmLoading = true
+          let httpurl = ''
+          let method = ''
+          if (!this.model.id) {
+            httpurl += this.url.add
+            method = 'post'
+          } else {
+            httpurl += this.url.edit
+            method = 'put'
+          }
+          httpAction(httpurl, this.model, method)
+            .then((res) => {
+              if (res.success) {
+                that.$message.success(res.message)
+                that.$emit('ok')
+              } else {
+                that.$message.warning(res.message)
+              }
+            })
+            .finally(() => {
+              that.confirmLoading = false
+              that.close()
+            })
+        } else {
+          return false
+        }
+      })
+    },
+    handleCancel() {
+      this.close()
+    },
+  },
+}
 </script>
 
 <style lang="less" scoped>
-  /**Button按钮间距*/
-  .ant-btn {
-    margin-left: 30px;
-    margin-bottom: 30px;
-    float: right;
-  }
- /**抽屉按钮样式*/
-  .drawer-bootom-button {
-    position: absolute;
-    bottom: -8px;
-    width: 100%;
-    border-top: 1px solid #e8e8e8;
-    padding: 10px 16px;
-    text-align: right;
-    left: 0;
-    background: #fff;
-    border-radius: 0 0 2px 2px;
-  }
+/**Button按钮间距*/
+.ant-btn {
+  margin-left: 30px;
+  margin-bottom: 30px;
+  float: right;
+}
+/**抽屉按钮样式*/
+.drawer-bootom-button {
+  position: absolute;
+  bottom: -8px;
+  width: 100%;
+  border-top: 1px solid #e8e8e8;
+  padding: 10px 16px;
+  text-align: right;
+  left: 0;
+  background: #fff;
+  border-radius: 0 0 2px 2px;
+}
 </style>

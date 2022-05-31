e9a18724064243b43f47941d795ae2679a885a14
iliweii <chall21@yeah.net>
2022-05-31 09:38:07 +0800

    结业典礼接口完善

--- vstms/jeecg-boot-module-system/src/main/java/org/jeecg/modules/graduation/mapper/GraduationCeremonyMapper.java
+++ vstms/jeecg-boot-module-system/src/main/java/org/jeecg/modules/graduation/mapper/GraduationCeremonyMapper.java
@@ -1,17 +1,28 @@
 package org.jeecg.modules.graduation.mapper;
 
-import java.util.List;
-
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.ibatis.annotations.Param;
 import org.jeecg.modules.graduation.entity.GraduationCeremony;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
  * @Description: 结业典礼
  * @Author: jeecg-boot
- * @Date:   2022-05-31
+ * @Date: 2022-05-31
  * @Version: V1.0
  */
 public interface GraduationCeremonyMapper extends BaseMapper<GraduationCeremony> {
+
+    /**
+     * 分页查询
+     *
+     * @param page         分页参数
+     * @param queryWrapper 查询构造器
+     * @return 分页结果
+     */
+    IPage<GraduationCeremony> selectPage(Page<GraduationCeremony> page, @Param(Constants.WRAPPER) QueryWrapper<GraduationCeremony> queryWrapper);
 
 }

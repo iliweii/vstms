03e01b68bd552c7c512baad3bf7818d91a7b0af0
iliweii <chall21@yeah.net>
2022-05-31 09:38:25 +0800

    结业典礼界面

---
+++ vstms-front/src/views/graduation/modules/GraduationCeremonyBaseModal.vue
@@ -0,0 +1,171 @@
+<template>
+  <a-drawer
+    :title="title"
+    :maskClosable="true"
+    :width="drawerWidth"
+    :destroyOnClose="true"
+    placement="right"
+    :closable="true"
+    @close="handleCancel"
+    :visible="visible"
+    style="height: 100%; overflow: auto; padding-bottom: 53px"
+  >
+    <template slot="title">
+      <div style="width: 100%; height: 30px">
+        <span>{{ title }}</span>
+        <span style="display: inline-block; width: calc(100% - 51px); padding-right: 10px; text-align: right">
+          <a-button @click="toggleScreen" icon="appstore" style="height: 20px; width: 20px; border: 0px"></a-button>
+        </span>
+      </div>
+    </template>
+
+    <a-alert :message="`${object.classNo_dictText}(${object.classNo}) 的结业典礼`" type="success" />
+    <a-row :gutter="24" :style="{ marginTop: '5px' }">
+      <a-col :md="8">
+        <span :style="{ padding: '15px' }">创建人：{{ object.createBy_dictText }}</span>
+      </a-col>
+      <a-col :md="8">
+        <span :style="{ padding: '15px' }">创建时间：{{ object.createTime }}</span>
+      </a-col>
+      <a-col :md="8">
+        <span :style="{ padding: '15px' }">当前状态：{{ object.status_dictText }}</span>
+      </a-col>
+    </a-row>
+
+    <!-- 标签页 -->
+    <a-tabs :activeKey="activeKey" @tabClick="tabClick">
+      <a-tab-pane key="1" tab="结业典礼方案">
+        <graduation-plan
+          :object="object"
+          :disabled="object.status != '1'"
+          :key="key + 1"
+          @close="handleCancel"
+          @change="handleChange"
+        />
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="结业典礼记录">
+        <graduation-record
+          :object="object"
+          :disabled="object.status != '2'"
+          :key="key + 2"
+          @close="handleCancel"
+          @change="handleChange"
+        />
+      </a-tab-pane>
+    </a-tabs>
+  </a-drawer>
+</template>
+
+<script>
+import { getDictItemsFromCache } from '@/api/api'
+import GraduationPlan from './GraduationCeremonyPlan.vue' // 结业典礼方案
+import GraduationRecord from './GraduationCeremonyRecord.vue' // 结业典礼记录
+
+export default {
+  name: 'GraduationBaseModal',
+  components: {
+    GraduationPlan,
+    GraduationRecord,
+  },
+  data() {
+    return {
+      drawerWidth: '70%',
+      title: '操作',
+      visible: false,
+      disableSubmit: false,
+      activeKey: '1',
+      key: String(new Date()),
+      object: {}, // 对象
+    }
+  },
+  methods: {
+    // 窗口最大化切换
+    toggleScreen() {
+      if (this.modaltoggleFlag) {
+        this.modalWidth = window.innerWidth
+      } else {
+        this.modalWidth = 800
+      }
+      this.modaltoggleFlag = !this.modaltoggleFlag
+    },
+    /**
+     * Tab页签切换事件
+     */
+    tabClick(params) {
+      this.activeKey = params
+    },
+    async add() {
+      this.visible = true
+    },
+    async edit(record) {
+      this.visible = true
+      record.description = null
+      this.object = record
+    },
+    handleCancel() {
+      this.object = {}
+      this.visible = false
+      this.$emit('ok')
+    },
+    handleChange(object) {
+      getDictItemsFromCache('training_opening_ceremony_status')
+        .filter((x) => x.value === object.status)
+        .forEach((x) => {
+          object.status_dictText = x.text
+        })
+      this.object = object
+      // 刷新key
+      this.key = String(new Date())
+    },
+  },
+}
+</script>
+
+<style scoped lang="less">
+.top_button {
+  padding-bottom: 10px;
+  .ant-btn {
+    margin-right: 10px;
+  }
+}
+
+/deep/.ant-modal-footer {
+  text-align: center;
+}
+/deep/.ant-col .ant-form-item-label {
+  display: inline-block;
+  text-align: right;
+}
+/deep/.table-page-search-wrapper .ant-form-inline .ant-form-item {
+  margin-bottom: 0px;
+}
+/deep/.ant-form-item-label {
+  text-align: center;
+}
+/deep/.ant-row .ant-form-item {
+  margin-bottom: 0px;
+}
+/deep/.ant-row .ant-form-item .ant-form-explain {
+  position: absolute;
+}
+/deep/.ant-form-item-label,
+/deep/.ant-form-item-control {
+  line-height: 35px;
+}
+/deep/.ant-btn > .anticon + span,
+.ant-btn > span + .anticon {
+  margin-left: 0;
+}
+.drawer-bootom-button {
+  position: absolute;
+  bottom: -8px;
+  width: 100%;
+  border-top: 1px solid #e8e8e8;
+  padding: 10px 16px;
+  text-align: right;
+  left: 0;
+  background: #fff;
+  border-radius: 0 0 2px 2px;
+  z-index: 100;
+}
+</style>
---
+++ vstms-front/src/views/graduation/modules/GraduationCeremonyPlan.vue
@@ -0,0 +1,94 @@
+<template>
+  <a-card :bordered="false">
+    <a-spin :spinning="false">
+      <a-form-model ref="form" :model="model" :rules="validatorRules">
+        <a-row>
+          <a-col :md="12">
+            <a-form-model-item :labelCol="{ span: 4 }" :wrapperCol="{ span: 18 }" prop="holdDate" label="举行日期">
+              <a-date-picker v-model="model.holdDate" :disabled="disabled" :style="{ width: '100%' }" />
+            </a-form-model-item>
+          </a-col>
+          <a-col :md="12">
+            <a-form-model-item :labelCol="{ span: 4 }" :wrapperCol="{ span: 16 }" prop="referNum" label="参与人数">
+              <a-input v-model="model.referNum" suffix="人" :disabled="disabled" />
+            </a-form-model-item>
+          </a-col>
+          <a-col :md="24">
+            <a-form-model-item :labelCol="{ span: 2 }" :wrapperCol="{ span: 20 }" prop="remark" label="备注">
+              <a-textarea
+                v-model="model.remark"
+                placeholder="请输入备注"
+                :autoSize="{ minRows: 5, maxRows: 10 }"
+                :disabled="disabled"
+              />
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+      </a-form-model>
+
+      <!-- 附件 -->
+      <j-file-upload
+        ref="jFileUpload"
+        :disabled="disabled"
+        businessType="graduation_ceremony_plan"
+        :objectId="object.classNo"
+        label="方案"
+        text="点击添加结业典礼方案"
+        :labelColumn="{ span: 2 }"
+        :wrapperColumn="{ span: 20 }"
+      />
+    </a-spin>
+
+    <!-- 完成按钮 -->
+    <div :style="{ marginTop: '15px' }">
+      <a-button @click="() => handleSubmit(model)" type="primary" :disabled="disabled" icon="check"
+        >完成结业典礼方案上传</a-button
+      >
+      <div class="ant-form-extra" :style="{ fontSize: '12px' }">
+        完成后方可执行后续步骤（结业典礼记录），完成后不可撤销
+      </div>
+    </div>
+  </a-card>
+</template>
+
+<script>
+import { putAction } from '@/api/manage'
+
+export default {
+  name: 'GraduationCeremonyPlan',
+  components: {},
+  props: {
+    // 1. 对象数据
+    object: Object,
+    // 通用禁用标识
+    disabled: {
+      type: Boolean,
+      default: true,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      model: this.object,
+      validatorRules: {},
+      url: {
+        edit: '/graduation/graduationCeremony/edit',
+      },
+    }
+  },
+  methods: {
+    handleSubmit(param) {
+      const that = this
+      param.status = '2'
+      putAction(that.url.edit, param).then((res) => {
+        that.$message.success('提交成功')
+        that.object.status = '2'
+        that.$emit('change', that.object)
+      })
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+</style>
